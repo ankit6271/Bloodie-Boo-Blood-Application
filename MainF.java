c92bd8d8b950a8a25affd6e99f4673ab8134fb3c
Ankit <48017526+ankit6271@users.noreply.github.com>
2020-05-30 00:13:45 +0530

    Add files via upload

---
+++ MainF.java
@@ -0,0 +1,8 @@
+import Project1.DonorOrReceiver;
+//import Project1.Person;
+//import Project1.
+public class MainF {
+    public static void main(String[] args) throws Exception {
+    	new DonorOrReceiver();
+    }
+}
